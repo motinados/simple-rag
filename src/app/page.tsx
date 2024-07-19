92d66d278f126045b518cad455ef513ab34659fa
motinados <motinados@gmail.com>
2024-07-20 06:35:49 +0900

    feat: show results

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,15 +1,23 @@
 "use client";
 import { generateTextWithRAG, generateTextWithoutRAG } from "@/lib/actions";
+import { useState } from "react";
 
 export default function Home() {
+  const [resultWithRAG, setResultWithRAG] = useState("");
+  const [resultWithoutRAG, setResultWithoutRAG] = useState("");
+
   const run = async () => {
+    setResultWithRAG("Loading...");
+    setResultWithoutRAG("Loading...");
+
     const input = "What is my favorite food?";
     const [result1, result2] = await Promise.all([
       generateTextWithRAG(input),
       generateTextWithoutRAG(input),
     ]);
-    console.log(result1);
-    console.log(result2);
+
+    setResultWithRAG(result1);
+    setResultWithoutRAG(result2);
   };
 
   return (
@@ -20,6 +28,14 @@ export default function Home() {
       >
         Run
       </button>
+      <div className="flex flex-col items-center">
+        <h2 className="text-xl font-bold">With RAG</h2>
+        <p className="text-gray-500">{resultWithRAG}</p>
+      </div>
+      <div className="flex flex-col items-center">
+        <h2 className="text-xl font-bold">Without RAG</h2>
+        <p className="text-gray-500">{resultWithoutRAG}</p>
+      </div>
     </main>
   );
 }
